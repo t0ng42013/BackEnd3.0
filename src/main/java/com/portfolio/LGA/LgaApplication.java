c725c59cf982cd64e6f5653e29f16f8c8e7cfbbd
t0ng42013 <tonga88@live.com.ar>
2023-01-25 17:02:57 -0300

    primer commit

---
+++ src/main/java/com/portfolio/LGA/LgaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.LGA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LgaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/LGA/LgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.LGA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
