c98c91c405e633833ac90fc3dde9de0137e8dc75
gastón_Alonso() <93102359+t0ng42013@users.noreply.github.com>
2023-02-01 11:53:05 -0300

    Corsgit

--- src/main/java/com/portfolio/LGA/config/CorsConfig.java
+++ src/main/java/com/portfolio/LGA/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/login")
-                        .allowedOrigins("https://portfolio3-2.onrender.com")
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .exposedHeaders("*");
 
